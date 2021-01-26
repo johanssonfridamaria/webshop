3036a27c00d2fdb91015f682af32c7c23b7d4473
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-26 17:25:29 +0100

    added orderController

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,11 @@ const express = require('express');
 // Synkronize with our app
 const app = express();
 
+const adminController = require('./controllers/adminController')
 const userController = require('./controllers/userController');
 const productController = require('./controllers/productController');
+const orderController = require('./controllers/orderController');
+const orderSchema = require('./models/orders/orderSchema');
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
@@ -20,8 +23,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //controllers
+app.use('/api/admin', adminController);
 app.use('/api/users', userController);
 app.use('/api/products', productController);
+app.use('/api/orders', orderController);
 
 //export the whole file
 module.exports = app;
