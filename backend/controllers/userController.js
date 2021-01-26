be3a38b4db5532edcbc16ccd7a6fe8fa2eed0e83
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-26 19:01:42 +0100

    added verifyToken

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 const userModel = require('../models/users/userModel');
+const auth = require('../authentication/auth');
 
-router.post('/register', auth,  userModel.registerUser);
+router.post('/register', auth.verifyToken,  userModel.registerUser);
 
 router.post('/login', userModel.loginUser);
 
-router.get('/', auth , userModel.getUsers);
+router.get('/', auth.verifyToken , userModel.getUsers);
 
-router.patch('/:email', auth , userModel.updateUser);
+router.patch('/:email', auth.verifyToken , userModel.updateUser);
 
-router.delete('/:email', auth ,userModel.deleteUser);
+router.delete('/:email', auth.verifyToken ,userModel.deleteUser);
 
 module.exports = router;
