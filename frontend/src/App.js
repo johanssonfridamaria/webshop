d6a3e293b7abce903f5e79fd5c7b52d9885ddf29
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-26 10:41:21 +0100

    imported browser router, switch and route från react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div >
+    <BrowserRouter >
 
-    </div>
+    </BrowserRouter>
   );
 }
 
