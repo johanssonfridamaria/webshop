fa1672dc30dc3613ab9bdcb9bd9029009b36e7b3
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-26 10:55:39 +0100

    added routes to views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './views/Home';
+import Products from './views/Products';
+import Login from './views/Login';
+import ProductDetails from './views/ProductDetails';
+
 
 function App() {
   return (
     <BrowserRouter >
-
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/products" component={Products}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/products/:id" component={ProductDetails}/>
+      </Switch>
     </BrowserRouter>
   );
 }
