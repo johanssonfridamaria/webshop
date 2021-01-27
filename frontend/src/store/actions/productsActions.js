29a8a9a52816d3d51a8a08f78acf8cd38632f948
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-27 20:12:16 +0100

    added action functions

--- frontend/src/store/actions/productsActions.js
+++ frontend/src/store/actions/productsActions.js
@@ -1,6 +1,40 @@
 import actiontypes from '../actiontypes';
 import axios from '../../axios';
 
-export const 
+export const fetchInit = payload => {
+  return {
+    type: actiontypes().products.fetchInit,
+    payload
+  }
+}
+
+export const fetchProducts = () => {
+  
+  return async dispatch => {
+    const res = await axios.get('/products');
+    console.log(res)
+    if (res.status === 200) {
+      dispatch(fetchSuccess(res.data))
+    } else {
+      dispatch(fetchFail())
+    } 
+  }
+}
+
+export const fetchSuccess = products => {
+  console.log(products)
+  return {
+    type: actiontypes().products.fetchSuccess,
+    payload: products
+  }
+}
+
+export const fetchFail = payload => {
+  return {
+    type: actiontypes().products.fetchFail,
+    payload
+  }
+}
+
 
 
