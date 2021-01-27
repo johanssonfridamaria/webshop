750b9ceefafb61db797379726949e92cba2d3061
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-27 16:25:43 +0100

    added initialState and switch

--- frontend/src/store/reducers/productsReducer.js
+++ frontend/src/store/reducers/productsReducer.js
@@ -1,3 +1,36 @@
 import actiontypes from '../actiontypes';
 import axios from '../../axios'
 
+const initState = {
+  loading: false,
+  error: false,
+  products: null,
+  product: null
+}
+
+const productsReducer = (state = initState, action) => {
+  switch (action.type) {
+    case actiontypes().products.fetchInit:
+      return {
+        ...state,
+        loading: true
+      }
+    case actiontypes().products.fetchSuccess:
+      return {
+        products: action.payload,
+        loading: false,
+        error: false
+      }
+      case actiontypes().products.fetchFail:
+        return  {
+          ...state,
+          loading: false,
+          error: true
+        }
+    default:
+      return state;
+  }
+
+}
+
+export default productsReducer;
