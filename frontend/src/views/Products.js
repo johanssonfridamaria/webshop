518fa2d859474c73f30d8d4f8cec3deb90d6a1e4
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-27 20:13:51 +0100

    called dispatch fetch products in useeffect

--- frontend/src/views/Products.js
+++ frontend/src/views/Products.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchInit, fetchProducts } from '../store/actions/productsActions';
+import productsReducer from '../store/reducers/productsReducer';
+
 
 const Products = () => {
+
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.productsReducer.loading);
+  const products = useSelector(state => state.productsReducer.products);
+
+
+  useEffect(() => {
+    // dispatch(fetchInit())
+    console.log(products)
+    dispatch(fetchProducts());
+  }, [dispatch])
+
+
   return (
     <div>
-      Products
+      {
+        loading
+          ? <p>Loading...</p>
+          : products.map(product => (
+            <p>{product.name}</p>
+          ))
+      }
     </div>
   )
 }
