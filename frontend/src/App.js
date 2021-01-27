66a18704c1e4ed6ca81bdb9e5af4d4ea9b390e22
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-27 10:16:47 +0100

    imported navbar and footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,24 @@ import Home from './views/Home';
 import Products from './views/Products';
 import Login from './views/Login';
 import ProductDetails from './views/ProductDetails';
+import Navbar from './components/navigation/Navbar';
+import Footer from './components/navigation/Footer';
 
 
 function App() {
   return (
     <BrowserRouter >
+
+      <Navbar />
+
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/products" component={Products}/>
         <Route exact path="/login" component={Login}/>
         <Route exact path="/products/:id" component={ProductDetails}/>
       </Switch>
+
+      <Footer />
     </BrowserRouter>
   );
 }
