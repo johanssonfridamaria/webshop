a9c9719c7e48cbb3e2027da1ac41c935460cbde7
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-27 10:38:38 +0100

    added navbar content

--- frontend/src/components/navigation/Navbar.js
+++ frontend/src/components/navigation/Navbar.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className="navbar">
+      <div className="container">
+        <NavLink exact to="/" className="logo">WEBSHOP</NavLink>
+        <div>
+          <NavLink exact to="/" className="nav-link">Home</NavLink>
+          <NavLink exact to="/products" className="nav-link">Products</NavLink>
+        </div>
+        <div>
+          <NavLink exact to="/login" className="nav-link">Sign in</NavLink>
+          <span className="cart">
+            <i class="fas fa-shopping-bag"></i>
+            <p>(0)</p>
+          </span>
+          {/* <NavLink exact to="/myorders">myorders</NavLink> */}
+        </div>
+      </div>
+    </nav>
   )
 }
 
