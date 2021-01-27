270ef217509deec953082d85ab590311a7d9903f
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-27 20:11:41 +0100

    required multer and defined storage and upload

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,13 +2,27 @@ const router = require('express').Router();
 const productModel = require('../models/products/productModel');
 const auth = require('../authentication/auth');
 
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function(req,file,cb){
+    cb(null, config.PROJECT_DIR+'/public/media') // here we specify the destination . in this case I specified the current directory
+  },
+  filename: function(req, file, cb){
+    console.log(file);
+    cb(null, file.originalname) // here we specify the file saving name . in this case I specified the original file name
+  }
+})
+
+const upload = multer({storage: storage})
+
+
 router.get('/', productModel.getProducts);
 
 router.get('/:id', productModel.getOneProduct);
 
-router.post('/new', auth.verifyToken, productModel.createProduct);
+router.post('/new', auth.verifyToken, upload.single('image'), productModel.createProduct);
 
-router.patch('/:id', auth.verifyToken, productModel.updateProduct);
+router.patch('/:id', auth.verifyToken, upload.single('image'), productModel.updateProduct);
 
 router.delete('/:id', auth.verifyToken, productModel.deleteProduct);
 
