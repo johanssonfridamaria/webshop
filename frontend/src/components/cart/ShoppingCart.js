7af490b535b09e43c3c958ec0637b44ee4ea8dab
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-28 17:03:47 +0100

    added shoppingcart totalquant, amount, check out  btn and imported useSelector

--- frontend/src/components/cart/ShoppingCart.js
+++ frontend/src/components/cart/ShoppingCart.js
@@ -1,11 +1,38 @@
-import React from 'react'
-import CartProduct from './CartProduct'
+import React from 'react';
+import CartProduct from './CartProduct';
+import { useSelector, useDispatch } from 'react-redux';
+import cartReducer from '../../store/reducers/cartReducer';
 
 const ShoppingCart = () => {
-  return (
+
+  const shoppingCart = useSelector(state => state.cartReducer.cart);
+  const totalCartAmount = useSelector(state => state.cartReducer.totalCartAmount);
+  const totalCartQuantity = useSelector(state => state.cartReducer.totalCartQuantity);
+
+  const empty = (
     <div>
-      <h1>cart</h1>
-      <CartProduct />
+      Your cart is empty
+    </div>
+  )
+
+  return (
+    <div className="shopping-cart__inner">
+      {
+        shoppingCart && shoppingCart.map(product => (
+          <CartProduct key={product._id} item={product} />
+        ))
+      }
+      {
+        !shoppingCart && empty
+      }
+
+      <div className="shopping-cart__inner-bottom">
+        <div className="shopping-cart__total">
+          <p>{totalCartQuantity} PCS</p>
+          <p>Total: {totalCartAmount} SEK</p>
+        </div>
+        <button className="btn-primary">Checkout</button>
+      </div>
     </div>
   )
 }
