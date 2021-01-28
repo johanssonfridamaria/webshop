d3d812d30a2c648bfc87f2efd9e5dc5e7b89ad6a
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-28 19:55:20 +0100

    added product image, name, and quantity and price

--- frontend/src/components/cart/CartProduct.js
+++ frontend/src/components/cart/CartProduct.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const cartProduct = () => {
+const cartProduct = ({ item }) => {
   return (
-    <div>
-      cartProduct
+    <div className="shopping-cart__product">
+      <img src={item.image} alt={item.name} />
+      <div className="shopping-cart__product-price">
+        <small><strong>{item.name}</strong></small>
+        <small>{item.quantity} x {item.price} SEK</small>
+      </div>
+
+
     </div>
   )
 }
