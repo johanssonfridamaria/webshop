72650bc7f872bd3cca96ad9ae7c4caa045610f37
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-28 19:54:14 +0100

    added quantity

--- backend/models/products/productSchema.js
+++ backend/models/products/productSchema.js
@@ -3,16 +3,17 @@ const ObjectId = mongodb.Schema.Types.ObjectId;
 
 const productSchema = mongodb.Schema({
 
-    _id:            ObjectId,
-    name:           { type: String, required: true, unique: true },
-    brand:          { type: String, required: true },
-    short:          { type: String, required: true },
-    desc:           { type: String, required: true },
-    price:          { type: Number, required: true },
-    image:          { type: String, required: true },
-    
-    created:        {type:Date, default: Date.now()},
-    modified:        {type:Date, default: Date.now()}
+    _id: ObjectId,
+    name: { type: String, required: true, unique: true },
+    brand: { type: String, required: true },
+    short: { type: String, required: true },
+    desc: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    quantity: { type: Number, default: 0 },
+
+    created: { type: Date, default: Date.now() },
+    modified: { type: Date, default: Date.now() }
 });
 
 module.exports = mongodb.model('Product', productSchema);
