d4b73315dc202c5a9486f85d7e0f56d01402b8be
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-28 16:38:21 +0100

    added function to hide and show cart

--- frontend/src/components/navigation/Navbar.js
+++ frontend/src/components/navigation/Navbar.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import ShoppingCart from '../cart/ShoppingCart';
 
 const Navbar = () => {
+
+  const [isOpen, setisOpen] = useState('false')
+
+  const toogleBag = () => {
+    setisOpen(!isOpen);
+
+    const move = document.querySelector('#app');
+    const nav = document.querySelector('.shopping-cart-side')
+    console.log(move)
+    if (isOpen) {
+      nav.style.width = `${15}rem`
+      move.style.marginRight = `${15}rem`
+    } else {
+      move.style.marginRight = `${0}rem`
+      nav.style.width = `${0}rem`
+    }
+  }
+
+
   return (
     <nav className="navbar">
       <div className="container">
@@ -13,13 +33,14 @@ const Navbar = () => {
         </ul>
         <ul>
           <li><NavLink exact to="/login" activeClassName="link-active">Sign in</NavLink></li>
-          <li><span className="cart">
-            <i className="fas fa-shopping-bag"></i>
-            <span className="quantity">(0)</span>
-          </span>
-          <div className="shopping-cart-side">
-            Cart component here
-          </div>
+          <li>
+            <span className="cart">
+              <i className="fas fa-shopping-bag" onClick={toogleBag}></i>
+              <span className="quantity">(0)</span>
+            </span>
+            <div className='shopping-cart-side'>
+              <ShoppingCart />
+            </div>
           </li>
         </ul>
       </div>
