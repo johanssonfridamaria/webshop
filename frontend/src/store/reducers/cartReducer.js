c2479103ed3fae0cb9721bf47bde32127a89f6ac
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-28 16:38:08 +0100

    created cartReducer

--- frontend/src/store/reducers/cartReducer.js
+++ frontend/src/store/reducers/cartReducer.js
@@ -5,3 +5,34 @@ const initState = {
   totalCartQuantity: 0,
   totalCartAmount: 0
 }
+
+const cartReducer = (state = initState, action) => {
+
+  switch (action.type) {
+    case actiontypes().cart.add:
+
+      return state
+    case actiontypes().cart.remove:
+
+      return state
+    case actiontypes().cart.delete:
+
+      return state
+    case actiontypes().cart.clear:
+      return {
+        ...state,
+        cart: [],
+        totalCartAmount: 0,
+        totalCartQuantity: 0,
+      }
+
+    default:
+
+
+      return state
+
+  }
+
+}
+
+export default cartReducer;
