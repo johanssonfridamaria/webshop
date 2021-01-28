615094d093ff3744311f94a36fe2d08a0c947ac8
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-28 14:05:06 +0100

    added link to product

--- frontend/src/components/products/ProductCard.js
+++ frontend/src/components/products/ProductCard.js
@@ -1,24 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="card__wrap--inner">
-      <div className="card">
-        <img src={product.image} alt={product.name} />
-        <div className="mt-1">
-          <h3>{product.name}</h3>
+    <Link to={`/products/${product._id}`}>
+      <div className="card__wrap--inner">
+        <div className="card">
+          <img src={product.image} alt={product.name} />
+          <div className="mt-1">
+            <h3>{product.name}</h3>
+          </div>
+          <div className="card__brand">
+            <h5 >{product.brand}</h5>
+          </div>
+          <div className="flexible py-1">
+          </div>
+          <div className="price">
+            <p>{product.price} SEK</p>
+          </div>
+          {/* <button className="btn-secondary">Add to cart</button> */}
         </div>
-        <div className="card__brand">
-          <h5 >{product.brand}</h5>
-        </div>
-        <div className="flexible py-1">
-        </div>
-        <div className="price">
-          <p>{product.price} SEK</p>
-        </div>
-        {/* <button className="btn-secondary">Add to cart</button> */}
       </div>
-    </div>
+    </Link>
   )
 }
 
