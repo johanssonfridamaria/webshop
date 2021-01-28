cf26bb0b99b262fa60326c1a373afe33edad55e6
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-28 16:31:40 +0100

    created shoppingcart component

---
+++ frontend/src/components/cart/ShoppingCart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CartProduct from './CartProduct'
+
+const ShoppingCart = () => {
+  return (
+    <div>
+      <h1>cart</h1>
+      <CartProduct />
+    </div>
+  )
+}
+
+export default ShoppingCart;
