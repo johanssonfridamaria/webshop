9582da04d3e8c0db8484e300f553509070a3589a
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-29 09:41:26 +0100

    added quantity state and created add to cart action

--- frontend/src/store/reducers/cartReducer.js
+++ frontend/src/store/reducers/cartReducer.js
@@ -2,14 +2,30 @@ import actiontypes from '../actiontypes';
 
 const initState = {
   cart: [],
+  quantity: 1,
   totalCartQuantity: 0,
   totalCartAmount: 0
 }
 
 const cartReducer = (state = initState, action) => {
+  let itemIndex;
+
 
   switch (action.type) {
     case actiontypes().cart.add:
+      let product = action.payload;
+
+      itemIndex = state.cart.findIndex(item => item._id === product._id)
+
+      if (itemIndex < 0) {
+        product.quantity += state.quantity
+        state.cart = [...state.cart, product]
+      } else {
+        state.cart[itemIndex].quantity += state.quantity
+      }
+
+      state.totalCartQuantity = getTotalQuantity(state.cart);
+      state.totalCartAmount = getTotalAmount(state.cart);
 
       return state
     case actiontypes().cart.remove:
@@ -34,5 +50,27 @@ const cartReducer = (state = initState, action) => {
   }
 
 }
+
+const getTotalQuantity = cart => {
+  let totalQuantity = 0;
+
+  cart.forEach(product => {
+    totalQuantity += product.quantity
+  })
+
+  return totalQuantity;
+}
+
+const getTotalAmount = cart => {
+  let totalAmount = 0;
+
+  cart.forEach(product => {
+    totalAmount += product.price * product.quantity
+  })
+
+  return totalAmount;
+}
+
+
 
 export default cartReducer;
