b2db6bb2be8a49837a619a3093834f17f66a7af5
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-29 10:19:02 +0100

    moved link and added "add to cart" btn

--- frontend/src/components/products/ProductCard.js
+++ frontend/src/components/products/ProductCard.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { addToCart } from '../../store/actions/cartActions';
+import { useDispatch } from 'react-redux';
 
 const ProductCard = ({ product }) => {
+
+  const dispatch = useDispatch();
+
   return (
-    <Link to={`/products/${product._id}`}>
-      <div className="card__wrap--inner">
-        <div className="card">
+    <div className="card__wrap--inner">
+      <div className="card">
+        <Link to={`/products/${product._id}`}>
           <img src={product.image} alt={product.name} />
           <div className="mt-1">
             <h3>{product.name}</h3>
           </div>
-          <div className="card__brand">
-            <h5 >{product.brand}</h5>
-          </div>
-          <div className="flexible py-1">
-          </div>
-          <div className="price">
-            <p>{product.price} SEK</p>
-          </div>
-          {/* <button className="btn-secondary">Add to cart</button> */}
+        </Link>
+        <div className="card__brand">
+          <h5 >{product.brand}</h5>
+        </div>
+        <div className="flexible py-1">
+        </div>
+        <div className="price">
+          <p>{product.price} SEK</p>
+          <button className="btn-secondary" onClick={() => { dispatch(addToCart(product)) }}>Add to cart</button>
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
 
