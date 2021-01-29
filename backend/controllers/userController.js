f96c18a0f8824a149aa6c3ca1c7eaf9e65e5f265
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-29 14:40:07 +0100

    removed verify token on register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 const userModel = require('../models/users/userModel');
 const auth = require('../authentication/auth');
 
-router.post('/register', auth.verifyToken,  userModel.registerUser);
+router.post('/register', userModel.registerUser);
 
 router.post('/login', userModel.loginUser);
 
-router.get('/', auth.verifyToken , userModel.getUsers);
+router.get('/', auth.verifyToken, userModel.getUsers);
 
-router.patch('/:email', auth.verifyToken , userModel.updateUser);
+router.patch('/:email', auth.verifyToken, userModel.updateUser);
 
-router.delete('/:email', auth.verifyToken ,userModel.deleteUser);
+router.delete('/:email', auth.verifyToken, userModel.deleteUser);
 
 module.exports = router;
