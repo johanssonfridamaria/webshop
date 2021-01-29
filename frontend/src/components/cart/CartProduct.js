ce36de8c950b4ccdb88d01c73c83489a78e57796
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-29 10:17:55 +0100

    added buttons

--- frontend/src/components/cart/CartProduct.js
+++ frontend/src/components/cart/CartProduct.js
@@ -4,9 +4,14 @@ const cartProduct = ({ item }) => {
   return (
     <div className="shopping-cart__product">
       <img src={item.image} alt={item.name} />
-      <div className="shopping-cart__product-price">
+      <div className="shopping-cart__price">
         <small><strong>{item.name}</strong></small>
         <small>{item.quantity} x {item.price} SEK</small>
+        <div className="shopping-cart__buttons">
+          <button className="btn-round">+</button>
+          <button className="btn-round">-</button>
+          <button className="btn-round trash"><i class="far fa-trash-alt"></i></button>
+        </div>
       </div>
 
 
