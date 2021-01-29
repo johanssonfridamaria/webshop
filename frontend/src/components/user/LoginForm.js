84daa3fd51b60a06215410908ea4066599b00d14
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-29 17:20:39 +0100

    added login action

--- frontend/src/components/user/LoginForm.js
+++ frontend/src/components/user/LoginForm.js
@@ -1,40 +1,62 @@
-import React, { useRef } from 'react';
-import { useDispatch } from 'react-redux';
-import { login } from '../../store/actions/authActions';
+import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../../store/actions/authActions';
 
 const LoginForm = () => {
 
   const dispatch = useDispatch();
   const history = useHistory();
+  let error = useSelector(state => state.authReducer.error);
+  let isAuthenticated = useSelector(state => state.authReducer.isAuthenticated)
 
   const email = useRef();
   const password = useRef();
 
   const onSub = e => {
+    console.log(error)
     e.preventDefault();
-    dispatchEvent(login(email.current.value, password.current.value));
-    email.current.value = '';
-    password.current.value = '';
-    history.push('/')
+    dispatch(login(email.current.value, password.current.value));
+    // console.log(error)
+    // if (error) {
+    //   email.current.value = '';
+    //   password.current.value = '';
+    //   history.push('/')
+    // }
   }
 
+  useEffect(() => {
+
+    console.log('effect', isAuthenticated)
+    if (isAuthenticated) {
+      email.current.value = '';
+      password.current.value = '';
+      history.push('/')
+    }
+  }, [])
+
+
   return (
-    <form onSubmit={onSub}>
-      <div className="form__group mb-1">
-        <label className="form__label" htmlFor="email">Email:</label>
-        <input type="email" id="email" ref={email} className="form__input" />
+    <div>
+      <div className="mb-1">
+        <h1>Sign In</h1>
       </div>
+      <form onSubmit={onSub}>
+        <div className="form__group mb-1">
+          <label className="form__label" htmlFor="email">Email:</label>
+          <input type="email" id="email" ref={email} className="form__input" />
+        </div>
 
-      <div className="form__group mb-3">
-        <label className="form__label" htmlFor="password">Password:</label>
-        <input type="password" id="password" className="form__input" ref={password} />
-      </div>
-
-      <p>Email or password is not correct</p>
-
-      <button type="submit" className="btn-secondary">Sign in</button>
-    </form>
+        <div className="form__group mb-1">
+          <label className="form__label" htmlFor="password">Password:</label>
+          <input type="password" id="password" className="form__input" ref={password} />
+        </div>
+        {error &&
+          <div className="form__error"><small>Email or password is incorrect!</small></div>
+        }
+        <button type="submit" className="btn-secondary mt-3">Sign in</button>
+      </form>
+    </div>
   )
 }
 
