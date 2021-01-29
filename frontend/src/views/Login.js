bfe3550812a9a5c74772fbdf6096a8cf18659aca
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-29 17:21:26 +0100

    moved headline to form comp

--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -2,12 +2,12 @@ import React from 'react'
 import LoginForm from '../components/user/LoginForm'
 
 const Login = () => {
+
+
+
   return (
     <div className="form">
       <div>
-        <div className="mb-1">
-          <h1>Sign In</h1>
-        </div>
         <LoginForm />
       </div>
     </div>
