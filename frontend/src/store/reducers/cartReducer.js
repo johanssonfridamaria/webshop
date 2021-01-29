5e98ca5fda1941ac24e124fcdf6a45730a9c56d6
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-29 14:02:41 +0100

    added reducers and saved state to localstorage

--- frontend/src/store/reducers/cartReducer.js
+++ frontend/src/store/reducers/cartReducer.js
@@ -1,10 +1,19 @@
 import actiontypes from '../actiontypes';
+import jwt from 'jsonwebtoken';
+require('dotenv').config({ path: '/backend' });
+
+const secretKey = process.env.SECRET_KEY;
 
 const initState = {
   cart: [],
-  quantity: 1,
+  // quantity: 1,
   totalCartQuantity: 0,
   totalCartAmount: 0
+}
+
+const delFromCart = id => {
+  initState.cart = initState.cart.filter(item => item._id !== id);
+  return initState.cart;
 }
 
 const cartReducer = (state = initState, action) => {
@@ -18,32 +27,51 @@ const cartReducer = (state = initState, action) => {
       itemIndex = state.cart.findIndex(item => item._id === product._id)
 
       if (itemIndex < 0) {
-        product.quantity += state.quantity
+        product.quantity += 1
         state.cart = [...state.cart, product]
       } else {
-        state.cart[itemIndex].quantity += state.quantity
+        state.cart[itemIndex].quantity += 1
       }
 
       state.totalCartQuantity = getTotalQuantity(state.cart);
       state.totalCartAmount = getTotalAmount(state.cart);
 
+      localStorage.setItem('cart', JSON.stringify(state))
+
       return state
+
     case actiontypes().cart.remove:
 
-      return state
-    case actiontypes().cart.delete:
+      action.payload.quantity === 1
+        ? delFromCart(action.payload._id)
+        : action.payload.quantity -= 1
+
+      state.totalCartQuantity = getTotalQuantity(state.cart);
+      state.totalCartAmount = getTotalAmount(state.cart);
+
+      // localStorage.setItem('JAYew4vrGpzQe4fVe2NFVbpaMWaKJEB5', jwt.sign(state, secretKey))
+      localStorage.setItem('cart', JSON.stringify(state))
 
       return state
-    case actiontypes().cart.clear:
-      return {
-        ...state,
-        cart: [],
-        totalCartAmount: 0,
-        totalCartQuantity: 0,
-      }
+
+    case actiontypes().cart.delete:
+      delFromCart(action.payload)
+
+      state.totalCartQuantity = getTotalQuantity(state.cart);
+      state.totalCartAmount = getTotalAmount(state.cart);
+
+      // localStorage.setItem('JAYew4vrGpzQe4fVe2NFVbpaMWaKJEB5', jwt.sign(state, secretKey))
+      localStorage.setItem('cart', JSON.stringify(state))
+
+      return state
 
     default:
+      // let cart = jwt.decode(localStorage.getItem('JAYew4vrGpzQe4fVe2NFVbpaMWaKJEB5'))
+      let cart = JSON.parse(localStorage.getItem('cart'))
 
+      console.log(cart)
+      if (cart)
+        state = cart
 
       return state
 
@@ -70,6 +98,8 @@ const getTotalAmount = cart => {
 
   return totalAmount;
 }
+
+
 
 
 
