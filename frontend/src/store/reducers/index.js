539d6294d84d874de6ef5dafebf751414df5d5e6
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-30 21:14:45 +0100

    imported user- and orderReducer

--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from 'redux';
 import productsReducer from './productsReducer';
 import cartReducer from './cartReducer';
 import authReducer from './authReducer';
+import orderReducer from './oderReducer';
+import userReducer from './userReducer';
 
 export default combineReducers({
   productsReducer,
   cartReducer,
-  authReducer
+  authReducer,
+  orderReducer,
+  userReducer
 });
