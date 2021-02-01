3ccddf1741488a26c5d8cc363c6ee85fcf489e08
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-01 15:48:34 +0100

    sent user in token

--- backend/authentication/auth.js
+++ backend/authentication/auth.js
@@ -3,17 +3,17 @@ require('dotenv/config');
 
 const secretKey = process.env.SECRET_KEY;
 
-exports.generateToken = id => {
-    return jwt.sign({ id: id }, secretKey, { expiresIn: '1h' })
+exports.generateToken = user => {
+    return jwt.sign({ user: user }, secretKey, { expiresIn: '1h' })
 };
 
-exports.verifyToken = (req,res,next)=>{
-    try{
+exports.verifyToken = (req, res, next) => {
+    try {
         const token = req.headers.authorization.split(" ")[1];
         req.userData = jwt.verify(token, secretKey);
         next();
     }
-    catch{
+    catch {
         return res.status(401).json({
             statusCode: 401,
             status: false,
