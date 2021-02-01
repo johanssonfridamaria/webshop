2be857b59e9493b6824d1d7720297686d420ec57
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-01 16:01:46 +0100

    first commit

--- frontend/src/store/reducers/orderReducer.js
+++ frontend/src/store/reducers/orderReducer.js
@@ -1,1 +1,15 @@
 import actiontypes from '../actiontypes';
+
+const initState = {
+  orders: null,
+  order: null,
+  loading: false,
+}
+
+const orderReducer = (state = initState, action) => {
+
+
+
+}
+
+export default orderReducer;
---
+++ frontend/src/store/reducers/userReducer.js
@@ -0,0 +1,15 @@
+import actiontypes from '../actiontypes';
+
+const initState = {
+  userId: null,
+  role: null,
+  token: null,
+}
+
+const userReducer = (state = initState, action) => {
+
+
+
+}
+
+export default userReducer;
