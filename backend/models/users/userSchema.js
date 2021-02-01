c46518db1ba386a75fd4850491d8242aa7d52e0e
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-01 11:20:53 +0100

    removed userid

--- backend/models/users/userSchema.js
+++ backend/models/users/userSchema.js
@@ -1,16 +1,16 @@
 const mongodb = require('mongoose');
 const ObjectId = mongodb.Schema.Types.ObjectId;
 
-const userSchema= mongodb.Schema({
+const userSchema = mongodb.Schema({
 
-    _id: ObjectId,
-    firstName:      {type:String, required: true},
-    lastName:       {type:String, required: true},
-    email:          {type:String, required: true, unique:true},
-    passwordHash:   {type:String, required: true},
-    
-    created:        {type:Date, default: Date.now()},
-    modified:        {type:Date, default: Date.now()}
+    // _id: ObjectId,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+
+    created: { type: Date, default: Date.now() },
+    modified: { type: Date, default: Date.now() }
 });
 
 module.exports = mongodb.model('User', userSchema);
