fac82ad6d86fc90a992ca7aaff5bce858812bad4
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-02 17:07:47 +0100

    added content and send order function

--- frontend/src/components/checkOut/Summary.js
+++ frontend/src/components/checkOut/Summary.js
@@ -1,29 +1,66 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
-import CartProduct from '../cart/CartProduct';
-import CheckOutProd from './CheckOutProd';
+import CustomerInfo from './CustomerInfo';
+import { checkoutCart } from '../../store/actions/cartActions';
 
 const Summary = () => {
 
   const history = useHistory();
+  const dispatch = useDispatch();
   const shoppingCart = useSelector(state => state.cartReducer.cart);
   const totalCartAmount = useSelector(state => state.cartReducer.totalCartAmount);
   const totalCartQuantity = useSelector(state => state.cartReducer.totalCartQuantity);
   const isLoggedIn = useSelector(state => state.userReducer.token);
+  let error = useSelector(state => state.cartReducer.error);
+
+  const sendOrder = () => {
+    let order = {
+      shoppingCart,
+      totalCartAmount,
+      totalCartQuantity
+    }
+
+    if (isLoggedIn) {
+      dispatch(checkoutCart(order))
+      console.log('hej')
+    } else {
+      error = true
+    }
+  }
+
 
   return (
-    <div className="checkout__summary">
-      <p>Total Number of Articles {totalCartQuantity}</p>
-      <p>Total sum: {totalCartAmount} </p>
-
+    <div>
       {
         isLoggedIn
-          ? <button className="btn-primary" onClick={onCheckout}>Send order</button>
-          : <p>You have to sign in before sending the order <Link>Sign in</Link></p>
-
+          ? <CustomerInfo />
+          : <Link to="/login" className="btn-secondary">Sign in</Link>
       }
 
+      <div className="checkout__summary mt-2">
+        <div className="checkout__summary-item">
+          <p>Product cost</p>
+          <p>{totalCartAmount}</p>
+        </div>
+        <div className="checkout__summary-item">
+          <p>Shipping</p>
+          <p>0</p>
+        </div>
+        <div className="checkout__summary-item__sum">
+          <p>Total sum</p>
+          <p>{totalCartAmount}</p>
+        </div>
+        {
+          error && (
+            <p>You have to sign in before sending the order</p>
+          )
+        }
+
+        <button className="btn-secondary my-2" onClick={sendOrder}>Send order</button>
+
+        <small>Note. This is only a fictional shop. The order is only sent to the database</small>
+      </div>
     </div>
   )
 }
