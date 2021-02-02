ad746a25994cda0eb2815dfb354dafd3aa23678b
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-02 17:07:04 +0100

    removed verify token

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,7 +3,7 @@ const orderModel = require('../models/orders/orderModel');
 const auth = require('../authentication/auth');
 
 router.get('/', auth.verifyToken, orderModel.getOrders);
-router.get('/:id/orders', auth.verifyToken, orderModel.getOrderbyUserId);
+router.get('/:id/orders', orderModel.getOrderbyUserId);
 router.post('/new', orderModel.createOrder);
 
 module.exports = router;
