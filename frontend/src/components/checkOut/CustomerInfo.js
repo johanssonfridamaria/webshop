e00ddc8b74b2280f2da027ec9cc59708e561cb08
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-02 17:07:26 +0100

    imported user name and email from state

--- frontend/src/components/checkOut/CustomerInfo.js
+++ frontend/src/components/checkOut/CustomerInfo.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+
 
 const CustomerInfo = () => {
-  return (
-    <div>
 
+  const firstName = useSelector(state => state.userReducer.userFirstName);
+  const lastName = useSelector(state => state.userReducer.userlastName);
+  const userEmail = useSelector(state => state.userReducer.userEmail);
+
+
+  return (
+    <div className="checkout__customer">
+      <h3>Your info</h3>
+      <p>{firstName} {lastName}</p>
+      <p>{userEmail}</p>
     </div>
   )
 }
