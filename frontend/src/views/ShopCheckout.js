f550acfb5c2a54122f404500024b1676196c8db9
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-02 14:24:57 +0100

    created view

---
+++ frontend/src/views/ShopCheckout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ShoppingCart from '../components/cart/ShoppingCart'
+
+const ShopCheckout = () => {
+  return (
+    <div>
+      <ShoppingCart />
+    </div>
+  )
+}
+
+export default ShopCheckout;
