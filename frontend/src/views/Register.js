80da46e1d6b323bd6200ad2d463d372fdd1d8bca
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-02 10:51:33 +0100

    imported form component and added h1

--- frontend/src/views/Register.js
+++ frontend/src/views/Register.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import RegisterUser from '../components/user/RegisterUser';
 
 const Register = () => {
   return (
-    <div>
-
+    <div className="form">
+      <div>
+        <div className="mb-1">
+          <h1>Register</h1>
+        </div>
+        <RegisterUser />
+      </div>
     </div>
   )
 }
