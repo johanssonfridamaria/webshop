7e83132a1606aac98f69607078badee9b5428192
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-02 21:47:09 +0100

    created order actions

--- frontend/src/store/actions/orderActions.js
+++ frontend/src/store/actions/orderActions.js
@@ -1,15 +1,54 @@
 import actiontypes from '../actiontypes';
+import axios from '../../axios';
 
-export const checkout = order => {
+export const fetchInit = payload => {
   return {
-    type: actiontypes().orders.checkout,
-    payload: order
+    type: actiontypes().orders.fetchInit,
+    payload
   }
 }
 
-export const viewOrders = orders => {
+export const fetchUserOrders = id => {
+
+  return async dispatch => {
+    const res = await axios.get(`/orders/user/${id}`);
+    if (res.status === 200) {
+      dispatch(fetchSuccess(res.data))
+    } else {
+      dispatch(fetchFail())
+    }
+  }
+}
+
+// export const fetchOneProduct = id => {
+
+//   return async dispatch => {
+//     const res = await axios.get(`/products/${id}`);
+//     if (res.status === 200) {
+//       dispatch(fetchOneSuccess(res.data))
+//     } else {
+//       dispatch(fetchFail())
+//     }
+//   }
+// }
+
+export const fetchSuccess = orders => {
   return {
-    type: actiontypes().orders.viewOrders,
+    type: actiontypes().orders.fetchSuccess,
     payload: orders
   }
 }
+// export const fetchOneSuccess = order => {
+//   return {
+//     type: actiontypes().products.fetchOneSuccess,
+//     payload: product
+//   }
+// }
+
+export const fetchFail = payload => {
+  return {
+    type: actiontypes().orders.fetchFail,
+    payload
+  }
+}
+
