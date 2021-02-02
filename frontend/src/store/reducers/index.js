12198d41574e9a8d66da1e5d12a35611abfca71b
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-02 09:54:21 +0100

    removed authreducer

--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -1,14 +1,12 @@
 import { combineReducers } from 'redux';
 import productsReducer from './productsReducer';
 import cartReducer from './cartReducer';
-import authReducer from './authReducer';
-import orderReducer from './oderReducer';
+// import orderReducer from './orderReducer';
 import userReducer from './userReducer';
 
 export default combineReducers({
   productsReducer,
   cartReducer,
-  authReducer,
-  orderReducer,
+  // orderReducer,
   userReducer
 });
