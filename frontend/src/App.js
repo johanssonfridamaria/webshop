89d467c7b4daaf9e1292bc12f2c143aebba1b5dc
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-02 14:24:38 +0100

    changed order on routes to have "/" last, added new checkout view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import './scss/App.scss'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Products from './views/Products';
-import Login from './views/Login';
-import ProductDetails from './views/ProductDetails';
 import Navbar from './components/navigation/Navbar';
-import Footer from './components/navigation/Footer';
-import Checkout from './views/Checkout';
-import MyOrders from './views/MyOrders';
 import { UserRoute } from './routes/ProtectedRoute';
+import Login from './views/Login';
 import Register from './views/Register';
+import MyOrders from './views/MyOrders';
+import ProductDetails from './views/ProductDetails';
+import Products from './views/Products';
+import Footer from './components/navigation/Footer';
+import ShopCheckout from './views/ShopCheckout';
 
 function App() {
   return (
@@ -18,12 +18,12 @@ function App() {
         <div className="container">
           <div className="my-2 view">
             <Switch>
-              <Route exact path="/" component={Products} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
-              <Route exact path="/:id" component={ProductDetails} />
-              <Route exact path="/checkout" component={Checkout} />
+              <Route exact path="/checkout" component={ShopCheckout} />
               <UserRoute exact path="/orders" component={MyOrders} />
+              <Route exact path="/:id" component={ProductDetails} />
+              <Route exact path="/" component={Products} />
             </Switch>
           </div>
         </div>
