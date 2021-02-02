9ed55e0c9baa815d1b7558679c8fafea5217780d
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-02 21:48:01 +0100

    imported states from orderReducer and imported cartProduct comp

--- frontend/src/views/MyOrders.js
+++ frontend/src/views/MyOrders.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import CartProduct from '../components/cart/CartProduct';
+import { fetchInit, fetchUserOrders } from '../store/actions/orderActions';
+
 
 const MyOrders = () => {
+
+  const dispatch = useDispatch();
+  // const loading = useSelector(state => state.orderReducer.loading);
+  const orders = useSelector(state => state.orderReducer.orders);
+  const userId = useSelector(state => state.userReducer.userId)
+
+  useEffect(() => {
+    dispatch(fetchInit())
+    dispatch(fetchUserOrders(userId));
+  }, [dispatch])
+
+
   return (
     <div>
-      user orders
+      {/* {
+        loading && !orders && <p>Loading...</p>
+      } */}
+      {
+        orders && orders.map(product => (
+          <CartProduct item={product} key={product._id} />
+        ))
+      }
     </div>
   )
 }
