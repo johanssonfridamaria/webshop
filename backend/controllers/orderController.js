66a20f87ae4c01542aab4c16ef4edf68b9f48258
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-02 10:29:38 +0100

    removed verifytoken for create order temporarily

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const orderModel = require('../models/orders/orderModel');
 const auth = require('../authentication/auth');
 
-router.get('/', auth.verifyToken , orderModel.getOrders);
-router.get('/:id/orders', auth.verifyToken ,orderModel.getOrderbyUserId);
-router.post('/new', auth.verifyToken, orderModel.createOrder);
+router.get('/', auth.verifyToken, orderModel.getOrders);
+router.get('/:id/orders', auth.verifyToken, orderModel.getOrderbyUserId);
+router.post('/new', orderModel.createOrder);
 
 module.exports = router;
