03879fa3992b74785d7512a012b9b7d1c2d0127c
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-02 10:43:42 +0100

    added route to register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Footer from './components/navigation/Footer';
 import Checkout from './views/Checkout';
 import MyOrders from './views/MyOrders';
 import { UserRoute } from './routes/ProtectedRoute';
+import Register from './views/Register';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Switch>
               <Route exact path="/" component={Products} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
               <Route exact path="/:id" component={ProductDetails} />
               <Route exact path="/checkout" component={Checkout} />
               <UserRoute exact path="/orders" component={MyOrders} />
