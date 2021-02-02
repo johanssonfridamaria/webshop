7f4b7e88d13e1b7d2f1d2f5fac6a84d6dc58ff8a
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-02 21:47:23 +0100

    added states and switch

--- frontend/src/store/reducers/orderReducer.js
+++ frontend/src/store/reducers/orderReducer.js
@@ -1,13 +1,41 @@
 import actiontypes from '../actiontypes';
 
 const initState = {
+  loading: false,
+  error: false,
   orders: null,
   order: null,
-  loading: false,
 }
 
 const orderReducer = (state = initState, action) => {
+  switch (action.type) {
+    case actiontypes().orders.fetchInit:
+      return {
+        ...state,
+        loading: true
+      }
+    case actiontypes().orders.fetchSuccess:
+      return {
+        orders: action.payload,
+        loading: false,
+        error: false
+      }
+    case actiontypes().orders.fetchFail:
+      return {
+        ...state,
+        loading: false,
+        error: true
+      }
+    // case actiontypes().orders.fetchOneSuccess:
+    //   return {
+    //     order: action.payload,
+    //     loading: false,
+    //     error: false
+    //   }
 
+    default:
+      return state;
+  }
 
 
 }
