6e8cd55f11825d9b9fdf4b33e50c377ba937ce28
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-03 10:12:59 +0100

    imported ordertable

--- frontend/src/views/MyOrders.js
+++ frontend/src/views/MyOrders.js
@@ -1,33 +1,13 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import CartProduct from '../components/cart/CartProduct';
-import { fetchInit, fetchUserOrders } from '../store/actions/orderActions';
-
+import OrderTable from '../components/orders/OrderTable';
 
 const MyOrders = () => {
 
-  const dispatch = useDispatch();
-  const userId = useSelector(state => state.userReducer.userId)
-  const loading = useSelector(state => state.orderReducer.loading);
-  const orders = useSelector(state => state.orderReducer.orders);
-
-  useEffect(() => {
-    console.log(loading, orders, userId)
-    dispatch(fetchInit())
-    dispatch(fetchUserOrders(userId));
-  }, [dispatch, userId])
-
-
   return (
     <div>
-      {
-        loading && !orders && <p>Loading...</p>
-      }
-      {
-        orders && orders.map(order => (
-          <CartProduct item={product} key={product._id} />
-        ))
-      }
+      <div className="mb-2">
+        <h2>Your orders</h2>
+      </div>
+      <OrderTable />
     </div>
   )
 }
