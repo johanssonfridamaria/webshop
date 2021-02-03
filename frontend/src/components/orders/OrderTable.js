cc04cb654e85a1770671fed37665edfb546a6d75
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-03 11:17:51 +0100

    wrapped table in container w responsive class

--- frontend/src/components/orders/OrderTable.js
+++ frontend/src/components/orders/OrderTable.js
@@ -5,23 +5,25 @@ const OrderTable = ({ orders }) => {
 
 
   return (
-    <table className="order-table">
-      <thead>
-        <tr>
-          <th scope="col">Date</th>
-          <th scope="col">Order no.</th>
-          <th scope="col">Quantity</th>
-          <th scope="col">Total amount</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          orders && orders.map(order => (
-            <Order order={order} key={order._id} />
-          ))
-        }
-      </tbody>
-    </table>
+    <div className="myorders-container">
+      <table className="myorders__table">
+        <thead>
+          <tr>
+            <th scope="col">Date</th>
+            <th scope="col">Order no.</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Total amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            orders && orders.map(order => (
+              <Order order={order} key={order._id} />
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
