83d4cf67e8d3595163bd0759947fee0e330e5561
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-03 13:53:44 +0100

    created component

--- frontend/src/components/orders/ProductOrder.js
+++ frontend/src/components/orders/ProductOrder.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const ProductOrder = () => {
+const ProductOrder = ({ item }) => {
+
+
+
   return (
-    <div>
-      order details
+    <div className="orderdetails__product">
+      <div className="orderdetails__productName">
+        <img src={item.image} alt={item.name} />
+        <strong className="ml-1">{item.name}</strong>
+      </div>
+      <div>
+        <small>{item.quantity} pcs </small>
+      </div>
+      <div>
+        <small>{item.price}:-</small>
+      </div>
     </div>
   )
 }
