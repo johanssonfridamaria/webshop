91ec108558684684d9ee111d6da68c4555960746
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-10 22:23:25 +0100

    added toogleCart, updated add and remove to not change state and create a copy of state instead

--- frontend/src/store/reducers/cartReducer.js
+++ frontend/src/store/reducers/cartReducer.js
@@ -5,46 +5,66 @@ const initState = {
   totalCartQuantity: 0,
   totalCartAmount: 0,
   isOpen: false,
-  error: false,
+  error: false
 }
-
 
 const cartReducer = (state = initState, action) => {
   let itemIndex;
-
+  let nextState;
 
   switch (action.type) {
+
     case actiontypes().cart.add:
+
+      let newState = { ...state, cart: [...state.cart] };
+
       let product = action.payload;
 
       itemIndex = state.cart.findIndex(item => item._id === product._id)
 
       if (itemIndex < 0) {
         product.quantity += 1
-        state.cart = [...state.cart, product]
+        newState.cart = [...state.cart, product]
       } else {
-        state.cart[itemIndex].quantity += 1
+        newState.cart[itemIndex].quantity += 1
       }
 
-      state.totalCartQuantity = getTotalQuantity(state.cart);
-      state.totalCartAmount = getTotalAmount(state.cart);
+      newState.totalCartQuantity = getTotalQuantity(newState.cart);
+      newState.totalCartAmount = getTotalAmount(newState.cart);
 
-      localStorage.setItem('cart', JSON.stringify(state))
+      localStorage.setItem('cart', JSON.stringify(newState))
 
-      return state
+      newState.isOpen = true;
+      return newState
 
     case actiontypes().cart.remove:
+      // let nextState = { ...state, cart: [...state.cart] };
 
-      action.payload.quantity === 1
-        ? state.cart = state.cart.filter(item => item._id !== action.payload._id)
-        : action.payload.quantity -= 1
 
-      state.totalCartQuantity = getTotalQuantity(state.cart);
-      state.totalCartAmount = getTotalAmount(state.cart);
 
-      localStorage.setItem('cart', JSON.stringify(state))
 
-      return state
+      // action.payload.quantity === 1
+      //   ? nextState.cart = state.cart.filter(item => item._id !== action.payload._id)
+      //   : action.payload.quantity -= 1
+
+      // nextState.totalCartQuantity = getTotalQuantity(nextState.cart);
+      // nextState.totalCartAmount = getTotalAmount(nextState.cart);
+
+
+
+      nextState = {
+        ...state,
+        totalCartAmount: getTotalAmount(state.cart),
+        totalCartQuantity: getTotalQuantity(state.cart),
+        cart: [
+          ...(action.payload.quantity === 1 ?
+            state.cart.filter(x => x._id !== action.payload._id)
+            :
+            state.cart.map(x => x._id === action.payload._id ? { ...x, quantity: x.quantity - 1 } : x))
+        ]
+      };
+      localStorage.setItem('cart', JSON.stringify(nextState));
+      return nextState;
 
     case actiontypes().cart.delete:
       state.cart = state.cart.filter(item => item._id !== action.payload);
@@ -56,10 +76,9 @@ const cartReducer = (state = initState, action) => {
 
       return state
 
-    // case actiontypes().cart.openCart:
-    //   state.isOpen = !action.payload
-    //   return state
-
+    case actiontypes().cart.toggleCart:
+      state.isOpen = !state.isOpen
+      return state;
 
     case actiontypes().cart.clear:
       state.shoppingCart = []
