d71c21bb9dfa2ba1bd3cd29d1d0505b5f14369b6
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-10 15:57:13 +0100

    added orderconfirmation

---
+++ frontend/src/views/OrderConfirm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const OrderConfirm = () => {
+  return (
+    <div>
+      <h2>Thanks for your order!</h2>
+      <h5>This is only a fictional shop, the order was only sent to the database.</h5>
+      <h3>Thanks for checking out the shop!</h3>
+    </div>
+  )
+}
+
+export default OrderConfirm
